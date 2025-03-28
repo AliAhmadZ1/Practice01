3c43c9962cebffd349f60ba2542f82498e7dcfc7
aliali <aliali@test.com>
2025-03-28 18:00:59 +0300

    add book endpoint

--- src/main/java/com/example/practice01/Controller/BookController.java
+++ src/main/java/com/example/practice01/Controller/BookController.java
@@ -1,14 +1,15 @@
 package com.example.practice01.Controller;
 
 import com.example.practice01.ApiResponse.ApiResponse;
+import com.example.practice01.Model.Book;
 import com.example.practice01.Service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -24,4 +25,16 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(bookService.getBooks());
     }
 
+    @PostMapping("/add")
+    public ResponseEntity addBook(@RequestBody @Valid Book book, Errors errors) {
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(message));
+        }
+        if (bookService.addBook(book).equals("10") || bookService.addBook(book).equals("13"))
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Book is add successfully!"));
+        if (bookService.addBook(book).equals("id"))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse("Book is already exist.."));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse("Book doesn't match correct ISBN based on date"));
+    }
 }
